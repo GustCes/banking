55a00401a9496f6d2f56ca32b973b281c168fd56
unicesmag510 <gustavoimp14@gmail.com>
2025-09-03 16:22:59 -0500

    usuarios añadidos ocmo clases en models.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# escribir en singular primera mayuscula
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20, blank=True)
+    #email = models.EmailField(max_length=150, unique=True)
+    #password = models.TextField(max_length=128, unique=True)
+    #mobile_number = models.CharField(max_length=15, unique=True)
+    #address= models.CharField(max_length=255)
+    #status = models.BooleanField(default=True)
+    #created_at = models.DateTimeField(auto_now_add=True)
+    #updated_at = models.DateTimeField(auto_now_add=True)
+    #deleted_at = models.DateTimeField(null=True)
+
+    #def __str__(self):
+        #return self.username
