39206ed62589da0ccd72941fb0d8d90328473e52
unicesmag510 <gustavoimp14@gmail.com>
2025-09-05 12:50:24 -0500

    construccion de clases iniciada y notas en comandos actualizada

--- authentication/models.py
+++ authentication/models.py
@@ -6,13 +6,16 @@ class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20, blank=True)
     #email = models.EmailField(max_length=150, unique=True)
-    #password = models.TextField(max_length=128, unique=True)
+    #password = models.TextField(max_length=128, unique=True) #se que la contraseña no es unica pero quise hacerla por un meme que vi
     #mobile_number = models.CharField(max_length=15, unique=True)
     #address= models.CharField(max_length=255)
     #status = models.BooleanField(default=True)
     #created_at = models.DateTimeField(auto_now_add=True)
     #updated_at = models.DateTimeField(auto_now_add=True)
-    #deleted_at = models.DateTimeField(null=True)
 
-    #def __str__(self):
-        #return self.username
+class Countries(models.Model):
+    name = models.CharField(max_length=20)
+    abrev = models.CharField(max_length=5)
+    #created_at = models.DateTimeField(auto_now_add=True)
+    #updated_at = models.DateTimeField(auto_now=True)
+    #user_id = models.ForeignKey(User, on_delete=models.CASCADE) #ejemplo de llave foranea
