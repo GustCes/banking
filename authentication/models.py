13b9dd5be6c29a5a951c8e57751a159626e0cdee
VXG portatil <gustavoimp14@gmail.com>
2025-09-10 16:05:10 -0500

    correccion en los nombres de los modelos y llave foranea de city en user añadida

--- authentication/models.py
+++ authentication/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 # escribir en singular primera mayuscula
 
-class Countries(models.Model):
+class Country(models.Model):
     name = models.CharField(max_length=20)
     abrev = models.CharField(max_length=5)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,19 +11,19 @@ class Countries(models.Model):
 
 
 
-class Departments(models.Model):
+class Department(models.Model):
     name = models.CharField(max_length=20)
     abrev = models.CharField(max_length=5)
-    country_id = models.ForeignKey(Countries, on_delete=models.CASCADE)
+    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
 
 
-class Cities(models.Model):
+class City(models.Model):
     name = models.CharField(max_length=20)
     abrev = models.CharField(max_length=5)
-    department_id = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
@@ -35,6 +35,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=20, blank=True)
     email = models.EmailField(max_length=150, unique=True)
     password = models.TextField(max_length=128, unique=True) #se que la contraseña no es unica pero quise hacerla por un meme que vi
+    city_id = models.ForeignKey(City, on_delete=models.CASCADE)
     mobile_number = models.CharField(max_length=15, unique=True)
     address= models.CharField(max_length=255)
     status = models.BooleanField(default=True)
