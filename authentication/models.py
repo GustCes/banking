c18a5122149409b428a3a13e8b8203e977fe6d29
VXG portatil <gustavoimp14@gmail.com>
2025-09-10 15:56:37 -0500

    clases creadas, recordar cambiar la configuracion de servidor default al trabajar en el pc de la univerdidad

--- authentication/models.py
+++ authentication/models.py
@@ -19,26 +19,26 @@ class Departments(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
 
-##3
-#class Cities(models.Model):
-#    name = models.CharField(max_length=20)
-#    abrev = models.CharField(max_length=5)
-#    department_id = models.ForeignKey(Departments, on_delete=models.CASCADE)
-#    created_at = models.DateTimeField(auto_now_add=True)
-#    updated_at = models.DateTimeField(auto_now=True)
-#    deleted_at = models.DateTimeField(null=True, blank=True)
 
-##4
+class Cities(models.Model):
+    name = models.CharField(max_length=20)
+    abrev = models.CharField(max_length=5)
+    department_id = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20, blank=True)
-    #email = models.EmailField(max_length=150, unique=True)
-    #password = models.TextField(max_length=128, unique=True) #se que la contraseña no es unica pero quise hacerla por un meme que vi
-    #mobile_number = models.CharField(max_length=15, unique=True)
-    #address= models.CharField(max_length=255)
-    #status = models.BooleanField(default=True)
-    #created_at = models.DateTimeField(auto_now_add=True)
-    #updated_at = models.DateTimeField(auto_now_add=True)
-    #deleted_at = models.DateTimeField(null=True, blank=True)
+    email = models.EmailField(max_length=150, unique=True)
+    password = models.TextField(max_length=128, unique=True) #se que la contraseña no es unica pero quise hacerla por un meme que vi
+    mobile_number = models.CharField(max_length=15, unique=True)
+    address= models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
